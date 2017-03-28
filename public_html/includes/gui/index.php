2982c6882eaa5c238fe287d89aac9da84849f8da
Marcin van de Ven <marcinvandeven@gmail.com>
2017-03-28 11:35:37 +0200

    Added overview of gui

--- public_html/includes/gui/index.php
+++ public_html/includes/gui/index.php
@@ -3,5 +3,12 @@
   <title>Overview of DB</title>
 </head>
 <body>
+  <h1>Overview of the database</h1>
+  <h3>Actions</h3>
+  <a href="addPage.php">Add a new page</a><br>
+  <!--<a href="addDir.php">Add a new directory</a><br>-->
+  <a href="addTrans.php">Add a new translation</a>
+
+  <h3>Overview of pages</h3>
 </body>
 </html>
