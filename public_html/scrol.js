e65d8f28a8ac3930d08ff925d648a95aa234b308
plamenpasliev <plamen_pasliev@abv.bg>
2017-03-30 10:07:01 +0200

    Backtotop button working and menu scrolls for you now in cs page

--- public_html/scrol.js
+++ public_html/scrol.js
@@ -1,5 +1,6 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
+
   var fixmeTop = $('#fixedmenu').offset().top;
   $(window).scroll(function() {
     var currentScroll = $(window).scrollTop();
@@ -12,13 +13,20 @@ $( document ).ready(function() {
 
           });
           $('#fixedcontent').css({'left' : '0px'});
+          $('#backtotop').css({'display' : 'block'});
         } else {
           $('#fixedmenu').css({
             'position' : 'static',
             'width' : '90%',
           });
           $('#fixedcontent').css({'left' : '0px'});
+          $('#backtotop').css({'display' : 'none'});
+
   }
+});
+$("#backtotop").click(function(){
+  document.body.scrollTop = 0; // For Chrome, Safari and Opera
+  document.documentElement.scrollTop = 0; // For IE and Firefox
 });
 
 });
