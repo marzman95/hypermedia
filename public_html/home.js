3d94e55be5f653edc963b9b5fd33f8b3e3880063
Stijn <Stijnvr@ziggo.nl>
2017-03-31 20:13:47 +0200

    Mobile blocks
    Two rows of two panels have now been added for the mobile version. The
    background of these blocks is the picture that would normally be in the
    body of the panel. The text in the headings and the footers will jump to
    the next line at the exact same moment.

--- public_html/home.js
+++ public_html/home.js
@@ -1,11 +1,59 @@
 function checkSize() {
- 	if($(window).width() <= 755) {
-		document.getElementsByClassName("panel-footer")[0].innerHTML='Learn more about the bachelor programs';
-		document.getElementsByClassName("panel-footer")[1].innerHTML="Learn more about the pre-master's programs";
-		document.getElementsByClassName("panel-footer")[2].innerHTML="Learn more about the master's programs";
-		document.getElementsByClassName("panel-footer")[3].innerHTML="Learn more about the post-master's programs";
+	if ($(window).width() <= 400) {
+		document.getElementsByClassName("panel-heading")[4].innerHTML='Bachelor<br><br>';
+		document.getElementsByClassName("panel-heading")[5].innerHTML="Pre-<br>master";
+		document.getElementsByClassName("panel-heading")[6].innerHTML="Master<br><br>";
+		document.getElementsByClassName("panel-heading")[7].innerHTML="Post-<br>master";
+		document.getElementsByClassName("panel-footer")[4].innerHTML='Learn more<br>about<br>the<br>bachelor<br>programs';
+		document.getElementsByClassName("panel-footer")[5].innerHTML="Learn more<br>about<br>the pre-<br>master's<br>programs";
+		document.getElementsByClassName("panel-footer")[6].innerHTML="Learn more<br>about<br>the<br>master's<br>programs";
+		document.getElementsByClassName("panel-footer")[7].innerHTML="Learn more<br>about<br>the post-<br>master's<br>programs";
 		$('#footerlinks').hide();
 	    $('#footermenu').show();
+	    $('#mainblocks').hide();
+		$('#mainblocksmobile').show();
+	    document.getElementsByClassName("panelbachelor")[0].style.marginTop='5px';		
+	} else if ($(window).width() <= 450) {
+		document.getElementsByClassName("panel-heading")[4].innerHTML='Bachelor';
+		document.getElementsByClassName("panel-heading")[5].innerHTML="Pre-master";
+		document.getElementsByClassName("panel-heading")[6].innerHTML="Master";
+		document.getElementsByClassName("panel-heading")[7].innerHTML="Post-master";
+		document.getElementsByClassName("panel-footer")[4].innerHTML='Learn more<br>about<br>the<br>bachelor<br>programs';
+		document.getElementsByClassName("panel-footer")[5].innerHTML="Learn more<br>about<br>the pre-<br>master's<br>programs";
+		document.getElementsByClassName("panel-footer")[6].innerHTML="Learn more<br>about<br>the<br>master's<br>programs";
+		document.getElementsByClassName("panel-footer")[7].innerHTML="Learn more<br>about<br>the post-<br>master's<br>programs";
+		$('#footerlinks').hide();
+	    $('#footermenu').show();
+	    $('#mainblocks').hide();
+		$('#mainblocksmobile').show();
+	    document.getElementsByClassName("panelbachelor")[0].style.marginTop='5px';
+	} else if($(window).width() <= 550) {
+		document.getElementsByClassName("panel-heading")[4].innerHTML='Bachelor';
+		document.getElementsByClassName("panel-heading")[5].innerHTML="Pre-master";
+		document.getElementsByClassName("panel-heading")[6].innerHTML="Master";
+		document.getElementsByClassName("panel-heading")[7].innerHTML="Post-master";
+		document.getElementsByClassName("panel-footer")[4].innerHTML='Learn more about<br>the bachelor<br>programs';
+		document.getElementsByClassName("panel-footer")[5].innerHTML="Learn more about<br>the pre-master's<br>programs";
+		document.getElementsByClassName("panel-footer")[6].innerHTML="Learn more about<br>the master's<br>programs";
+		document.getElementsByClassName("panel-footer")[7].innerHTML="Learn more about<br>the post-master's<br>programs";
+		$('#footerlinks').hide();
+	    $('#footermenu').show();
+	    $('#mainblocks').hide();
+		$('#mainblocksmobile').show();
+	    document.getElementsByClassName("panelbachelor")[0].style.marginTop='5px';
+ 	} else if($(window).width() <= 755) {
+		document.getElementsByClassName("panel-heading")[4].innerHTML='Bachelor';
+		document.getElementsByClassName("panel-heading")[5].innerHTML="Pre-master";
+		document.getElementsByClassName("panel-heading")[6].innerHTML="Master";
+		document.getElementsByClassName("panel-heading")[7].innerHTML="Post-master";
+		document.getElementsByClassName("panel-footer")[4].innerHTML='Learn more about the<br>bachelor programs';
+		document.getElementsByClassName("panel-footer")[5].innerHTML="Learn more about the<br>pre-master's programs";
+		document.getElementsByClassName("panel-footer")[6].innerHTML="Learn more about the<br>master's programs";
+		document.getElementsByClassName("panel-footer")[7].innerHTML="Learn more about the<br>post-master's programs";
+		$('#footerlinks').hide();
+	    $('#footermenu').show();
+	    $('#mainblocks').hide();
+		$('#mainblocksmobile').show();
 	    document.getElementsByClassName("panelbachelor")[0].style.marginTop='5px';
 	} else if ($(window).width() <= 1000) {
 		document.getElementsByClassName("panel-footer")[0].innerHTML='Learn more about<br>the bachelor<br>programs';
@@ -14,6 +62,8 @@ function checkSize() {
 		document.getElementsByClassName("panel-footer")[3].innerHTML="Learn more about<br>the post-master's<br>programs";
 		$('#footermenu').hide();
 		$('#footerlinks').show();
+	    $('#mainblocks').show();
+		$('#mainblocksmobile').hide();
 	    document.getElementsByClassName("panelbachelor")[0].style.marginTop='0px';
 	} else if ($(window).width() <= 1450) {
 		document.getElementsByClassName("panel-footer")[0].innerHTML='Learn more about<br>the bachelor programs';
@@ -22,6 +72,8 @@ function checkSize() {
 		document.getElementsByClassName("panel-footer")[3].innerHTML="Learn more about<br>the post-master's programs";
 		$('#footermenu').hide();
 		$('#footerlinks').show();
+	    $('#mainblocks').show();
+		$('#mainblocksmobile').hide();
 	    document.getElementsByClassName("panelbachelor")[0].style.marginTop='0px';
 	} else {
 		document.getElementsByClassName("panel-footer")[0].innerHTML='Learn more about the bachelor programs';
@@ -30,6 +82,8 @@ function checkSize() {
 		document.getElementsByClassName("panel-footer")[3].innerHTML="Learn more about the post-master's programs";
 		$('#footermenu').hide();
 		$('#footerlinks').show();
+	    $('#mainblocks').show();
+		$('#mainblocksmobile').hide();
 	    document.getElementsByClassName("panelbachelor")[0].style.marginTop='0px';
 	}
 }
