29cce546a9444c6c9166dee86e3776c796eab6c5
plamenpasliev <lampard3003@gmail.com>
2017-04-04 13:40:04 +0200

    Home section completed

--- public_html/enrollment.php
+++ public_html/enrollment.php
@@ -3,12 +3,12 @@
 
 <html lang="en">
 <head>
-  <title>Post-master programs</title>
+  <title>Enrollment</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="home.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="scrol.js"></script>
     <script src="hamburger.js"></script>
@@ -41,43 +41,30 @@
   </div>
   </div>
 </div>
-<button type="button" id="backtotop" class="btn btn-default btn-lg btn-block" style="vertical-align:middle"><span>Back to top</span></button>
-
-<div id="slidermenu" >
-<button type="button" id="menuclose" onClick="doMoreThings()"><span class="glyphicon glyphicon-remove"></span></button>
-<div id="hidable">
-<button type="button" onClick="document.getElementById('phd').scrollIntoView();" class="menubtn tuefont">PHD</button>
-<button type="button" onClick="document.getElementById('pdeng').scrollIntoView();" class="menubtn tuefont">PDEng</button>
-
-</div>
-</div>
-
 <nav class="navbar navbar-default text-black">
-<button type=""button class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-Show Menu<span class="caret"></span>
-</button>
-<div class="collapse navbar-collapse" id="myNavbar">
-  <ul class="nav navbar-nav tuefont">
-  <li><a href="home.html">Home</a></li>
-  <li><a href="bachelor.php">Bachelor</a></li>
-  <li><a href="pre-master.php">Pre-Master</a></li>
-  <li><a href="master.php">Master</a></li>
-  <li><a href="post-master.php">Post-Master</a></li>
-</ul>
-</div>
+  <button type=""button class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+    Menu<span class="caret"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav tuefont">
+        <li><a href="home.html">Home</a></li>
+        <li><a href="bachelor.php">Bachelor</a></li>
+        <li><a href="pre-master.php">Pre-Master</a></li>
+        <li><a href="master.php">Master</a></li>
+        <li><a href="post-master.php">Post-Master</a></li>
+    </ul>
+  </div>
 </nav>
+
+
+
 <div class="container-fluid">
-<div class="row">
-<div class="col-sm-3"  id="more">
-<div class="center-block tuefont sidemenu" id="fixedmenu">
-    <button type="button" onClick="document.getElementById('phd').scrollIntoView();" class="btn btn-default btn-lg btn-block" style="vertical-align:middle"><span>PHD</span></button>
-    <button type="button" onClick="document.getElementById('pdeng').scrollIntoView();" class="btn btn-default btn-lg btn-block" style="vertical-align:middle"><span>PDEng</span></button>
+  <div class="row">
 
-    </div>
-</div>
-    <div class="col-sm-9 tuefont" id="fixedcontent">
 
-    <?php ls('phd'); ?>
+    <div class="col-sm-12 tuefont" id="fixedcontent">
+
+    <?php ls('enrolling'); ?>
 
     </div>
   </div>
