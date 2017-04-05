e1c4ecff4f020fce0e45d9d03d948d7359a13674
Stijn <Stijnvr@ziggo.nl>
2017-04-05 15:39:01 +0200

    Enhacement #15
    This should add a completely functioning menu to the search page.

--- public_html/search.php
+++ public_html/search.php
@@ -56,9 +56,34 @@
       </ul>
     </div>
   </nav>
+  
+  <div id="slidermenu" >
+  	<button type="button" id="menuclose" onClick="doMoreThings()"><span class="glyphicon glyphicon-remove"></span></button>
+    <div id="hidable">
+      <a href="enrollment.php"><button type="button" class="menubtn tuefont">Enrollment</button></a>
+      <a href="https://www.tue.nl/en/education/studying-at-tue/study-costs-scholarships-and-grants/tuition-fees-2016-2017/"><button type="button" class="menubtn tuefont">Tuition Fees</button></a>
+      <a href="studentsstatute.php"><button type="button" class="menubtn tuefont">Students Statute</button></a>
+      <a href="https://www.tue.nl/en/university/about-the-university/organization/support-services/ict-services/"><button type="button" class="menubtn tuefont">ICT Services</button></a>
+      <a href="http://mytue.tue.nl"><button type="button" class="menubtn tuefont">MyTue</button></a>
+      <a href="timeslots.php"><button type="button" class="menubtn tuefont">Timeslots</button></a>
+	  <a href="agenda.php"><button type="button" class="menubtn tuefont">Acedemic Year Agenda</button></a>
+	  <a href="https://www.tue.nl/"><button type="button" class="menubtn tuefont">Future Students</button></a>
+    </div>
+  </div>
+
   <div class="container-fluid">
     <div class="row">
       <div class="col-sm-3" id="more">
+        <div class="center-block tuefont sidemenu" id="fixedmenu">
+          <a href="enrollment.php" role="button" class="btn btn-default btn-lg btn-block font-smaller" style="vertical-align:middle"><span>Enrollment</span></a>
+          <a href="https://www.tue.nl/en/education/studying-at-tue/study-costs-scholarships-and-grants/tuition-fees-2016-2017/" role="button" class="btn btn-default btn-lg btn-block font-smaller" style="vertical-align:middle"><span>Tuition fees</span></a>
+          <a href="studentsstatute.php" role="button" class="btn btn-default btn-lg btn-block font-smaller" style="vertical-align:middle"><span>Students Statute</span></a>
+          <a href="https://www.tue.nl/en/university/about-the-university/organization/support-services/ict-services/" role="button" class="btn btn-default btn-lg btn-block font-smaller" style="vertical-align:middle"><span>ICT Services</span></a>
+          <a href="http://mytue.tue.nl" role="button" class="btn btn-default btn-lg btn-block font-smaller" style="vertical-align:middle"><span>MyTue</span></a>
+          <a href="timeslots.php" role="button" class="btn btn-default btn-lg btn-block font-smaller" style="vertical-align:middle"><span>Timeslots</span></a>
+          <a href="agenda.php" role="button" class="btn btn-default btn-lg btn-block font-smaller" style="vertical-align:middle"><span>Academic Year Agenda</span></a>
+          <a href="https://www.tue.nl/" role="button" class="btn btn-default btn-lg btn-block font-smaller" style="vertical-align:middle"><span>Future Students</span></a>
+        </div>
       </div>
 
       <div class="col-sm-9 tuefont" id="fixedcontent">
